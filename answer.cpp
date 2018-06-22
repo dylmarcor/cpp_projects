440a14eb6d5015185da8c9a598999ee879a8a83b
Dylmarcor <dylancorbett@me.com>
2018-06-21 23:56:15 -0700

    Simple Answer function

---
+++ answer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+string answerQuestion(string q)
+{
+    string a;
+    if (q == "Please?") {
+	a = "Okay!";
+    } else {
+	a = "You forgot the magic word!";
+    }
+    return a;
+}
