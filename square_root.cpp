7a7b1a4b16af55df8923dc7d6b2a8b324233ab1b
Dylmarcor <dylancorbett@icloud.com>
2018-06-22 23:51:29 -0700

    Complete square_root program

--- square_root.cpp
+++ square_root.cpp
@@ -2,3 +2,14 @@
 #include <cmath>
 
 using namespace std;
+
+int main()
+{
+    int x = 0;
+    while(x < 10) {
+	double y = sqrt((double)x);
+	cout << "The square root of " << x << " is " << y << endl;
+	x++;
+    }
+    return 0;
+}
